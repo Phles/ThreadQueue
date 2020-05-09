9e2c4887838b8aa08bfbac91c97e18523fd8a70d
mc <michael.t.colsch@outlook.com>
2020-05-08 21:02:42 -0500

    Add project files.

---
+++ ThreadQueue/ThreadQueue.hpp
@@ -0,0 +1,53 @@
+/*
+ *  Thread Queue Library. Runs functions on threads.
+ */
+
+#ifndef THREADQUEUE
+#define THREADQUEUE
+
+#include <condition_variable>
+#include <thread>
+#include <vector>
+#include <functional>
+#include <any>
+namespace Phles {
+	/*
+	 * Abstract class common to all tasks
+	 */
+	class Job {
+	public:
+		virtual void run() = 0;
+	};
+	
+	/*
+	 * Task to some function on another thread.
+	 */
+	template<typename Fn,typename ...Args>
+	class Task : Job{
+	public:
+		//Function to execute
+		std::function<Fn> function;
+		
+		//Arguments to be passed to the function
+		std::tuple<Args...> arguments;
+
+		//Constructor
+		Task(Fn FunctionAddr = nullptr,Args... args)
+			:function(FunctionAddr)
+		{
+			arguments = std::make_tuple(args...);
+		}
+
+		//Run the function
+		void run() override{
+			std::apply(function, arguments);
+		}
+
+	};
+
+	
+	
+	
+}
+
+#endif
---
+++ ThreadQueue/test.cpp
@@ -0,0 +1,31 @@
+#include <chrono>
+#include <iostream>
+#include "ThreadQueue.hpp"
+/*
+ *	Function that happens first
+ */
+int someWork(int seconds,int a) {
+	
+	std::this_thread::sleep_for(std::chrono::seconds(seconds));
+
+	std::cout << "Finished "<< a <<" at " << seconds << "s sleep on thread " << std::this_thread::get_id() << "\n";
+	return a + seconds;
+}
+
+/*
+ * Entry point for testing
+ */
+int main(void) {
+	int a = 2;
+
+	a += 1;	
+	
+	Phles::Task<int(int,int),int,int> t(someWork,a,0);
+	
+	t.run();
+	
+	std::cin.get();
+	return 0;
+
+	
+}
